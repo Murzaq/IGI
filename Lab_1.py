b405bc630fc63c2f9793933171513d7c06566b2c
Brigadir <murzikelite@gmail.com>
2023-03-30 23:06:21 +0300

    Task3

--- Lab_1.py
+++ Lab_1.py
@@ -6,3 +6,8 @@ operators = {"+":operator.add,
             "/":operator.truediv}
 def calc(x1,x2,op):
     return[op](x1, x2)
+def evens(nums_list):
+   for num in nums_list:
+         if num % 2 == 0:
+            print(num, end=' ')
+evens([5, 20, 21, 58, 3])
