3625a18e8c5216156db930cc08e1c63216cfff8f
Brigadir <murzikelite@gmail.com>
2023-03-30 23:04:08 +0300

    Task2

--- Lab_1.py
+++ Lab_1.py
@@ -1,1 +1,8 @@
+import operator
 print("Hello world")
+operators = {"+":operator.add,
+             "-":operator.sub,
+             "*":operator.mul,
+            "/":operator.truediv}
+def calc(x1,x2,op):
+    return[op](x1, x2)
